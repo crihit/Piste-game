3d0dc2bcfeaee449b2f8d92b8f258a559b2ba01e
crihit <rlagusgkr98@gmail.com>
2022-11-11 07:08:06 +0900

    setting project

---
+++ Assets/Scripts/Lobby/LobbyButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyButton : MonoBehaviour
+{
+    public void playButton()
+    {
+        Debug.Log("pushed play");
+    }
+    public void deckButton()
+    {
+        Debug.Log("pushed deck");
+    }
+    public void settingButton()
+    {
+        Debug.Log("pushed setting");
+    }
+    public void tutorialButton()
+    {
+        Debug.Log("pushed tutorial");
+    }
+}
